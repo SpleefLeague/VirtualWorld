e9780f416f1dec69d400443947386b816fe354f0
Jonas Balsfulland <jonas@balsfulland.net>
2017-10-04 15:58:20 +0200

    Block breaking not instant when player is in the air

--- src/main/java/com/spleefleague/virtualworld/protocol/PacketBlockBreakAdapter.java
+++ src/main/java/com/spleefleague/virtualworld/protocol/PacketBlockBreakAdapter.java
@@ -44,7 +44,7 @@ public class PacketBlockBreakAdapter extends PacketAdapter {
         if(affected == null) {
             return;
         }
-        if(ppcbd.getStatus() == PlayerDigType.STOP_DESTROY_BLOCK || (ppcbd.getStatus() == PlayerDigType.START_DESTROY_BLOCK && isInstantlyDestroyed(p, affected.getType()))) {
+        if(ppcbd.getStatus() == PlayerDigType.STOP_DESTROY_BLOCK || (p.isOnGround() && ppcbd.getStatus() == PlayerDigType.START_DESTROY_BLOCK && isInstantlyDestroyed(p, affected.getType()))) {
             Bukkit.getScheduler().runTask(VirtualWorld.getInstance(), () -> {
                 FakeBlockBreakEvent breakEvent = new FakeBlockBreakEvent(affected, event.getPlayer());
                 Bukkit.getPluginManager().callEvent(breakEvent);
